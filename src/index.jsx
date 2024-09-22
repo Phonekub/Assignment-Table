db416a7d5092c8fbb3f95627ad2bc6d7bc2cbca0
Phonekub <155616014+Phonekub@users.noreply.github.com>
2024-09-22 16:18:10 +0000

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Assignment_table from './Assignment_table'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Assignment_table/>
   </React.StrictMode>
 );
 
