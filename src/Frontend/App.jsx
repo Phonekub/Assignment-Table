f2085f3a9f4d54fbe4f307b68d45d47928164dd6
Phakin0384 <phakinza1824@gmail.com>
2024-11-05 05:36:33 +0700

    Add detail to app interface

--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -7,6 +7,16 @@ import Menubar from "./Menubar";
 // import Contact from "./Contact"; // แก้ไขเส้นทางถ้าจำเป็น
 // import Cart from "./Cart"; // เพิ่มถ้าคุณมีหน้า Cart
 import Settings from "./Setting"; // เพิ่มถ้าคุณมีหน้า Settings
+//import PetDetail from "./Detail";
+
+// const petDetail = [
+//     { id: 0, name: "Corgi", price: 30000, age: 2, gender: "Male", weight: 20, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Pembroke_Corgi_Image_001.jpg/1280px-Pembroke_Corgi_Image_001.jpg" },
+//     { id: 1, name: "Poodle", price: 50000, age: 4, gender: "Female", weight: 30, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Standard_poodle_apricot.jpg/1024px-Standard_poodle_apricot.jpg" },
+//     { id: 2, name: "Chihuahua", price: 32900, age: 2, gender: "Female", weight: 10, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Degaen.jpg/220px-Degaen.jpg" },
+//     { id: 3, name: "Pug", price: 54000, age: 5, gender: "Male", weight: 14, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Fawn_pug_2.5year-old.JPG/1280px-Fawn_pug_2.5year-old.JPG" },
+//     { id: 4, name: "Syberian Husky", price: 99900, age: 5, gender: "Female", weight: 30, img: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" },
+//     { id: 5, name: "Shiba", price: 46900, age: 3, gender: "Male", weight: 30, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Taka_Shiba.jpg/640px-Taka_Shiba.jpg" }
+//   ];
 
 function App() {
     return (
@@ -21,6 +31,7 @@ function App() {
                 <Route path="/setting" element={<Settings />} />
             </Routes>
         </Router>
+        
     );
 }
 
--- src/Frontend/Detail.jsx
+++ src/Frontend/Detail.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import './PetCard.css';
+import './Detail.css';
 
-const PetCard = ({ pet }) => {
+const PetDetail = ({ pet }) => {
   return (
     <div className="pet-card">
       <img 
-        src={pet.image} 
-        alt={pet.breed} 
+        src={pet.img} 
+        alt={pet.name} 
         className="pet-card-image" 
       />
-      <div className="pet-card-title">{pet.breed}</div>
+      <div className="pet-card-title">{pet.name}</div>
       <div className="pet-card-stars">★★★★★</div>
       <div className="pet-card-details">
         <div>
-          <p>Age: {pet.age}</p>
+          <p>Age: {pet.age} years</p>
           <p>Gender: {pet.gender}</p>
           <p>Weight: {pet.weight} kg</p>
         </div>
         <div>
-          <p>Pet price: {pet.price}</p>
+          <p>Pet price: {pet.price} Baht</p>
           <button className="pet-card-button">Purchase</button>
         </div>
       </div>
@@ -26,4 +26,4 @@ const PetCard = ({ pet }) => {
   );
 };
 
-export default PetCard;
+export default PetDetail;
