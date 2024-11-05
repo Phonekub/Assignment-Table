d44a3724174aa1504c658fa934a4c1416547fdc5
rungwikrai <oakkmmmkk22@gmail.com>
2024-11-05 09:15:08 +0700

    delete anything not important

--- src/Frontend/PetShop.jsx
+++ src/Frontend/PetShop.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menubar from "./Menubar";
-// import Home from "./Home"; // แก้ไขให้ถูกต้อง
-// import About from "./About"; // ต้องตรวจสอบให้แน่ใจว่าใช้ไฟล์ที่ถูกต้อง
-// import Services from "./Services"; // แก้ไขเส้นทางถ้าจำเป็น
-// import Contact from "./Contact"; // แก้ไขเส้นทางถ้าจำเป็น
 import Cart from "./Cart"; // เพิ่มถ้าคุณมีหน้า Cart
 import Fav from "./Fav"; // เพิ่มถ้าคุณมีหน้า Settings
 import Backend from "./Backend";
@@ -17,14 +13,10 @@ function App() {
             <Menubar />
             <Routes>
                 <Route path="/home" element={<Home />} />
-                {/* <Route path="/about" element={<About />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/contact" element={<Contact />} /> */}
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/favs" element={<Fav />} />
                 <Route path="/backend" element={<Backend />} />
                 <Route path="/detail/:id" element={<Detail/>} />
-                {/* <Route path="/detail" element={<Detail petDetail={petDetail}/>} /> */}
             </Routes>
         </Router>
 
